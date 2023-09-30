3ee0b343957d0b10c1687ee605fa38aea6101d18
thebluefish <sean.kaufman92@gmail.com>
2023-09-29 18:16:39 -0700

    cleaned up auto-registration export
    * included example's output for demonstration purposes

--- burrtype_derive/src/lib.rs
+++ burrtype_derive/src/lib.rs
@@ -16,11 +16,12 @@ pub fn linkme(_args: ProcTokenStream, input: ProcTokenStream) -> ProcTokenStream
 pub fn burr(_args: ProcTokenStream, input: ProcTokenStream) -> ProcTokenStream {
     let item = parse_macro_input!(input as DeriveInput);
     let name = &item.ident;
-    let fn_name = quote::format_ident!("ctor_add_{}_type_registration", name.to_string().to_snake_case());
+    let fn_name = quote::format_ident!("burr_add_{}_type_registration", name.to_string().to_snake_case());
 
     let ir_auto_registration = quote! {
         #[burrtype::linkme::distributed_slice(burrtype::TYPES)]
         #[linkme(crate = burrtype::linkme)]
+        #[doc(hidden)]
         fn #fn_name() -> burrtype::TypeRegistration {
             <#name as burrtype::GetTypeRegistration>::get_type_registration()
         }
